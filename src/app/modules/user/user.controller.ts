0565e997ff72a8a0c22221a3f2135ed7f0ba8172
pedrohma07 <pedrohma07@gmail.com>
2024-01-29 22:00:12 -0300

    Refactor user controller and extend base class

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,23 +1,13 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Param,
-  Delete,
-  Put,
-} from '@nestjs/common';
+import { Body, Param, Controller } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 
 import * as bcrypt from 'bcrypt';
 
-@Controller('')
-export class UserController {
+export class UserControllerBase {
   constructor(private readonly userService: UserService) {}
 
-  @Post('')
   async create(@Body() createUserDto: CreateUserDto) {
     const userExist = await this.findByEmail(createUserDto.email);
 
@@ -32,28 +22,31 @@ export class UserController {
     return this.userService.create(data);
   }
 
-  @Get()
   findAll() {
     return this.userService.findAll();
   }
 
-  @Get()
   findOne(@Param('id') id: string) {
     return this.userService.findOne(id);
   }
 
-  @Put()
   update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
     return this.userService.update(id, updateUserDto);
   }
 
-  @Delete()
   remove(@Param('id') id: string) {
     return this.userService.remove(id);
   }
 
-  @Get()
   findByEmail(@Param('email') email: string) {
     return this.userService.findByEmail(email);
+  }
+}
+
+// Para desativar a controller, basta não estendê-la em outra classe.
+@Controller()
+export class UserController extends UserControllerBase {
+  constructor(userService: UserService) {
+    super(userService);
   }
 }

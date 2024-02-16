2148c59feba97855bc611a814117ef8cb1e20ae6
pedrohma07 <pedrohma07@gmail.com>
2024-02-16 09:10:31 -0300

    Add global prefix and dynamic versioning to API

--- src/main.ts
+++ src/main.ts
@@ -5,26 +5,29 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix(`api/${process.env.APP_URL_VERSION}`);
 
   const config = new DocumentBuilder()
     .setTitle('Documentação Swagger - DineExpress')
     .setDescription('Documentação da API do DineExpress')
-    .setVersion('1.0')
+    .setVersion(process.env.APP_VERSION)
     .addBearerAuth()
-    .addTag('users')
+    .setBasePath(`/api/${process.env.APP_URL_VERSION}`)
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
-
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true, // remove campos que não estão no DTO
       transform: true, // transforma os dados em objetos
     }),
   );
+
   await app.listen(process.env.APP_PORT || 1000);
   const url = (await app.getUrl()).replace('[::1]', 'localhost');
-  console.log(`Application is running on: ${url}`);
+  console.log(
+    `Application is running on: ${url}/api/${process.env.APP_URL_VERSION}`,
+  );
 }
 bootstrap();
